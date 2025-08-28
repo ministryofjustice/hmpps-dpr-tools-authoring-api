2fcd00e0f36d50a06d28a0df698b91084cd70822
gavriil.gavriilidis <gavriil.gavriilidis@digital.justice.gov.uk>
2025-08-28 16:34:00 +0100

    DPR2-2138 remove github workflows

--- src/main/kotlin/uk/gov/justice/digital/hmpps/templatepackagename/ExampleResource.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/templatepackagename/ExampleResource.kt
@@ -3,7 +3,6 @@ package uk.gov.justice.digital.hmpps.templatepackagename
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.time.LocalDateTime
 
 @RestController
 @RequestMapping("/example")
